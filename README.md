e1a12d0d762b54e7d15742271559f8fb7e1e3586
alfianjau <alfianjauhari@gmail.com>
2020-04-08 21:10:41 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+retrospective and rebuilding
+
+yarn workspace site add gatsby react react-dom gatsby-theme-events
+yarn workspace gatsby-theme-events add -P react react-dom gatsby
+yarn workspace gatsby-theme-events add -D react react-dom gatsby
+yarn workspace site develop
---
+++ gatsby-theme-events/.cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Unicornique/Documents/My Document/Hakooi/boilerplate/gatsby-theme-hakooi/gatsby-theme-events/.cache/dev-404-page.js")))
+}
+
